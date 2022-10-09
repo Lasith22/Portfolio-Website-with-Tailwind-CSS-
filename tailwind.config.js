e91b8e7228b9d71d62b61bbe46b54d31cd6fa974
Lasith Bandara <81031378+Lasith22@users.noreply.github.com>
2022-10-09 11:55:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
